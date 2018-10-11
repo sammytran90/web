6faf2659f3e48530973f47bf64441903494a475f
sammytran90 <32162425+sammytran90@users.noreply.github.com>
2018-10-11 16:48:12 +0700

    submit
    webMVN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DepositPageComponent } from './deposit-page/deposit-page.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'deposit', component: DepositPageComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    DepositPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
